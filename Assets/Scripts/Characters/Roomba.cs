06ddcdb6f50dc3b1b67cc9a091fcae96d6d2dacd
ChauNTran <luvmuzik4life@gmail.com>
2020-01-31 23:45:55 -0500

    roomba movements with options to move with physics to push other rigidbodies

--- Assets/Scripts/Characters/Roomba.cs
+++ Assets/Scripts/Characters/Roomba.cs
@@ -8,35 +8,51 @@ public class Roomba : MonoBehaviour
     [SerializeField] private string RoombaHorizontalMovementButton = "Horizontal";
     [SerializeField] private string RoombaVerticalMovementButton = "Vertical";
     [SerializeField] private float speed = 1.5f;
+    [SerializeField] private bool moveWithPhysics = true;
     private Rigidbody2D rigid;
-    float x_value;
-    float y_value;
+
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Mathf.Abs(Input.GetAxis(RoombaHorizontalMovementButton)) > 0.1)
+        float x_value = 0;
+        float y_value = 0;
+        if (Mathf.Abs(Input.GetAxis(RoombaHorizontalMovementButton)) > 0.5)
         {
             x_value = Input.GetAxis(RoombaHorizontalMovementButton);
         }
-        else if (Mathf.Abs(Input.GetAxis(RoombaVerticalMovementButton)) > 0.1)
+        else if (Mathf.Abs(Input.GetAxis(RoombaVerticalMovementButton)) > 0.5)
         {
             y_value = Input.GetAxis(RoombaVerticalMovementButton);
         }
         else
         {
-            rigid.velocity = Vector2.zero;
-
+            if (moveWithPhysics)
+                rigid.velocity = Vector2.zero;
             return;
         }
-        MoveCharacter(x_value, y_value);
+        if (moveWithPhysics)
+            MoveRigidbody(x_value, y_value);
+        else
+            MoveTransform(x_value, y_value);
+
+
     }
-    void MoveCharacter(float xVal, float yVal)
+    void MoveRigidbody(float xVal, float yVal)
     {
         //Debug.Log($"xVal {xVal} yVal {yVal}");
-        rigid.AddForce(new Vector2(xVal * speed, yVal * speed));
+        //Vector2 movementVector = new Vector2(xVal, yVal);
+
+        //rigid.AddForce(movementVector, ForceMode2D.Force);
+        Vector2 movePosition = (Vector2)transform.position + (new Vector2(xVal, yVal) * 0.02f);
+        rigid.MovePosition(movePosition);
+    }
+    void MoveTransform(float xVal, float yVal)
+    {
+        transform.position += new Vector3(xVal * Time.deltaTime * speed, yVal * Time.deltaTime * speed, 0);
     }
 }
