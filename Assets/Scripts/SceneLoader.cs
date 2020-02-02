3e898a69d3710d2611db1665298377bc6e5798c5
CHICKENB0Y93 <russty93@gmail.com>
2020-02-02 15:07:27 -0500

    Added quit button

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -11,5 +11,12 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(index);
     }
 
-
+    public void QuitApp()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
